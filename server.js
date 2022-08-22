a5c8f9b7c8e919565a193091b84c8f59fc67310c
onaygizem <onayau@gmail.com>
2022-08-22 06:45:56 -0400

    - note taker app

---
+++ server.js
@@ -0,0 +1,94 @@
+// Dependencies
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const database = require("./db/db.json")
+
+// Express App
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Display index.html
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// Display notes.html
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+})
+
+// /api/notes get and post functionality
+app.route("/api/notes")
+    // Grab the notes list (this should be updated for every new note and deleted note.)
+    .get(function (req, res) {
+        res.json(database);
+    })
+
+    // Add a new note to the json db file.
+    .post(function (req, res) {
+        let jsonFilePath = path.join(__dirname, "/db/db.json");
+        let newNote = req.body;
+
+        // This allows the test note to be the original note.
+        let highestId = 99;
+        // This loops through the array and finds the highest ID.
+        for (let i = 0; i < database.length; i++) {
+            let individualNote = database[i];
+
+            if (individualNote.id > highestId) {
+                // highestId will always be the highest numbered id in the notesArray.
+                highestId = individualNote.id;
+            }
+        }
+        // This assigns an ID to the newNote. 
+        newNote.id = highestId + 1;
+        // We push it to db.json.
+        database.push(newNote)
+
+        // Write the db.json file again.
+        fs.writeFile(jsonFilePath, JSON.stringify(database), function (err) {
+
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Note is saved");
+        });
+        // Gives back the response, which is the user's new note. 
+        res.json(newNote);
+    });
+
+
+// Delete a note based on an ID
+app.delete("/api/notes/:id", function (req, res) {
+    let jsonFilePath = path.join(__dirname, "/db/db.json");
+    // request to delete note by id.
+    for (let i = 0; i < database.length; i++) {
+
+        if (database[i].id == req.params.id) {
+            // Splice takes i position, and then deletes the 1 note.
+            database.splice(i, 1);
+            break;
+        }
+    }
+    // Write the db.json file again.
+    fs.writeFileSync(jsonFilePath, JSON.stringify(database), function (err) {
+
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log("Your note was deleted!");
+        }
+    });
+    res.json(database);
+});
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
